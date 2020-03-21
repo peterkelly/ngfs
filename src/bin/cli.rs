bcec21f4715436b1367aca3c57846296a742b45c
Peter Kelly <peter@pmkelly.net>
2020-03-21 14:05:52 +0700

    Work

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -15,6 +15,8 @@ use torrent::util::{BinaryData};
 use torrent::protobuf::{PBufReader, VarInt};
 use torrent::cid::CID;
 use torrent::p2p::p2p_test;
+use rand::prelude::Rng;
+use rand::distributions::{Distribution, Uniform};
 
 fn get_argument<'a>(args: &'a [String], index: usize, name: &str) -> Result<&'a String, Box<dyn Error>> {
     match args.get(index) {
@@ -43,6 +45,69 @@ async fn p2p_command(args: &[String]) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn u64_to_varint(mut value: u64) -> Vec<u8> {
+    let mut bytes: Vec<u8> = Vec::new();
+    while value > 0 {
+        let seven = value & 0x7f;
+        bytes.push(seven as u8);
+        value = value >> 7;
+    }
+    let mut index = 0;
+    while index + 1 < bytes.len() {
+        bytes[index] = bytes[index] | 0x80;
+        index += 1;
+    }
+    return bytes;
+}
+
+async fn varint_command(args: &[String]) -> Result<(), Box<dyn Error>> {
+    let mut rng = rand::thread_rng();
+    let range = Uniform::new_inclusive(0, u64::max_value());
+    let mut generator = range.sample_iter(rng);
+    let mut count = 0;
+    for i in 0..100000 {
+        let raw: u64 = generator.next().unwrap();
+        let bits: u64 = generator.next().unwrap() % 63;
+        let value = raw >> bits;
+        // let value: u64 = 0x042e2d258cdae657;
+        // println!("value   = 0x{:016x}", value);
+        let varint_bytes = u64_to_varint(value);
+
+        // for (i, b) in varint_bytes.iter().enumerate() {
+        //     print!("{:08b}", b);
+        //     if i + 1 < varint_bytes.len() {
+        //         print!(" ");
+        //     }
+        // }
+        // println!();
+
+        let mut offset = 0;
+        match VarInt::read_from(&varint_bytes, &mut offset) {
+            Some(v) => {
+                let decoded_value = v.to_u64();
+                if value == decoded_value {
+                    println!("0x{:016x} OK", value);
+                }
+                else {
+                    return general_error(&format!("0x{:016x} != 0x{:016x}", value, decoded_value));
+                }
+            }
+            None => {
+                return general_error(&format!("0x{:016x} INVALID", value));
+            }
+        }
+        // let decoded = VarInt::read_from(&varint_bytes, &mut offset);
+        // match decoded {
+        //     Some(v) => {
+        //         println!("decoded = 0x{:016x}", v.to_u64());
+        //     }
+        //     None => {
+        //         println!("Decoding failed");
+        //     }
+        // }
+    }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -57,6 +122,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
         "p2p" => {
             p2p_command(&args[2..]).await?;
+            Ok(())
+        }
+        "varint" => {
+            varint_command(&args[2..]).await?;
             Ok(())
         }
         _ => {
--- src/p2p.rs
+++ src/p2p.rs
@@ -10,6 +10,8 @@ use tokio::net::{lookup_host};
 use std::net::SocketAddr;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
+// use native_tls;
+// use tokio_tls;
 use super::result::general_error;
 use super::util::{BinaryData, escape_string};
 use super::protobuf::{PBufReader, VarInt};
@@ -32,6 +34,47 @@ pub async fn p2p_test(server_addr_str: &str) -> Result<(), Box<dyn Error>> {
     let mut reader = PBufReader::new(received);
     let msg = reader.read_length_delimited()?.to_string()?;
     println!("msg = {}", escape_string(&msg));
+
+    let mut tosend: Vec<u8> = Vec::new();
+    let tosend_str = "/multistream/1.0.0\n";
+    let tosend_bytes = tosend_str.as_bytes();
+    tosend.push(tosend_bytes.len() as u8);
+    tosend.append(&mut Vec::from(tosend_bytes));
+    let w = stream.write(&tosend).await?;
+    println!("Sent {} bytes", w);
+
+
+
+
+
+    let mut tosend: Vec<u8> = Vec::new();
+    let tosend_str = "/tls/1.0.0\n";
+    let tosend_bytes = tosend_str.as_bytes();
+    tosend.push(tosend_bytes.len() as u8);
+    tosend.append(&mut Vec::from(tosend_bytes));
+    let w = stream.write(&tosend).await?;
+    println!("Sent {} bytes", w);
+
+    let r = stream.read(&mut buf).await?;
+    println!("Received {} bytes: {}", r, BinaryData(&buf[..r]));
+    let received = &buf[0..r];
+    let mut reader = PBufReader::new(received);
+    let msg = reader.read_length_delimited()?.to_string()?;
+    println!("msg = {}", escape_string(&msg));
+
+
+    // let cx = native_tls::TlsConnector::builder()
+    //     .min_protocol_version(Some(native_tls::Protocol::Tlsv13))
+    //     .max_protocol_version(Some(native_tls::Protocol::Tlsv13))
+    //     .build()?;
+    // let cx = tokio_tls::TlsConnector::from(cx);
+
+    // let mut tls_stream = cx.connect("localhost", stream).await?;
+    // println!("TLS Connection established");
+
+
+
+
 
     // let mut offset = 0;
     // let msg_len = match VarInt::read_from(&received, &mut offset) {
