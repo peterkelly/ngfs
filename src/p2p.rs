39ca5ddecad673f97230016fba485cabe8914cc4
Peter Kelly <peter@pmkelly.net>
2020-03-21 16:39:58 +0700

    Work

--- src/p2p.rs
+++ src/p2p.rs
@@ -16,9 +16,62 @@ use super::result::{GeneralError, general_error};
 use super::util::{BinaryData, escape_string};
 use super::protobuf::{PBufReader, VarInt};
 
+#[derive(Debug)]
+enum KeyType {
+    RSA = 0,
+    Ed25519 = 1,
+    Secp256k1 = 2,
+    ECDSA = 3,
+}
+
+struct PublicKey {
+    key_type: KeyType,
+    data: Vec<u8>,
+}
+
+impl PublicKey {
+    fn from_pb(raw_data: &[u8]) -> Result<PublicKey, Box<dyn Error>> {
+        let mut key_type: Option<KeyType> = None;
+        let mut data: Option<Vec<u8>> = None;
+
+        let mut reader = PBufReader::new(&raw_data);
+        while let Some(field) = reader.read_field()? {
+            // println!("offset 0x{:04x}, field_number {:2}, data {:?}",
+            //     field.offset, field.field_number, field.data);
+            match field.field_number {
+                1 => {
+                    let key_type_int = field.data.to_u64()?;
+                    key_type = match key_type_int {
+                        0 => Some(KeyType::RSA),
+                        1 => Some(KeyType::Ed25519),
+                        2 => Some(KeyType::Secp256k1),
+                        3 => Some(KeyType::ECDSA),
+                        _ => {
+                            return general_error(&format!("Unknown key type: {}", key_type_int));
+                        }
+                    }
+                }
+                2 => {
+                    data = Some(Vec::from(field.data.to_bytes()?));
+                }
+                _ => {
+                }
+            }
+        }
+
+        let key_type: KeyType = key_type.ok_or_else(|| GeneralError::new(&format!("Missing field: key_type")))?;
+        let data: Vec<u8> = data.ok_or_else(|| GeneralError::new(&format!("Missing field: data")))?;
+
+        Ok(PublicKey {
+            key_type,
+            data,
+        })
+    }
+}
+
 struct Propose {
     rand: Vec<u8>,
-    pubkey: Vec<u8>,
+    pubkey: PublicKey,
     exchanges: Vec<String>,
     ciphers: Vec<String>,
     hashes: Vec<String>,
@@ -67,7 +120,7 @@ impl Propose {
 
         Ok(Propose {
             rand,
-            pubkey,
+            pubkey: PublicKey::from_pb(&pubkey)?,
             exchanges: exchanges.split(',').map(|s| String::from(s)).collect(),
             ciphers: ciphers.split(',').map(|s| String::from(s)).collect(),
             hashes: hashes.split(',').map(|s| String::from(s)).collect(),
@@ -231,7 +284,11 @@ pub async fn p2p_test(server_addr_str: &str) -> Result<(), Box<dyn Error>> {
     println!("Propose");
 
     println!("    rand = {}", BinaryData(&propose.rand));
-    println!("    pubkey = {}", BinaryData(&propose.pubkey));
+    println!("    pubkey =");
+
+    println!("        type = {:?}", propose.pubkey.key_type);
+    println!("        data = {}", BinaryData(&propose.pubkey.data));
+
     // println!("    exchanges = {}", propose.exchanges);
     // println!("    ciphers = {}", propose.ciphers);
     // println!("    hashes = {}", propose.hashes);
@@ -248,6 +305,11 @@ pub async fn p2p_test(server_addr_str: &str) -> Result<(), Box<dyn Error>> {
     for item in propose.hashes.iter() {
         println!("        {}", item);
     }
+
+    println!();
+    println!("pubkey");
+    // print_fields(&propose.pubkey)?;
+
 
 
     // let cx = native_tls::TlsConnector::builder()
