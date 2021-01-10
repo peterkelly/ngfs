550cbf37c39bdf47b7ab5c8c3b333191a390783b
Peter Kelly <peter@pmkelly.net>
2021-01-10 23:24:16 +0700

    Manually construct ClientHello handshake

--- src/bin/tls_test.rs
+++ src/bin/tls_test.rs
@@ -14,33 +14,97 @@ use torrent::binary::{BinaryReader, FromBinary, BinaryWriter, ToBinary};
 use torrent::tls::types::handshake::*;
 use torrent::tls::types::extension::*;
 use torrent::tls::types::record::*;
-
- // uint16 ProtocolVersion;
- //      opaque Random[32];
-
- //      uint8 CipherSuite[2];    /* Cryptographic suite selector */
-
- //      struct {
- //          ProtocolVersion legacy_version = 0x0303;    /* TLS v1.2 */
- //          Random random;
- //          opaque legacy_session_id<0..32>;
- //          CipherSuite cipher_suites<2..2^16-2>;
- //          opaque legacy_compression_methods<1..2^8-1>;
- //          Extension extensions<8..2^16-1>;
- //      } ClientHello;
+use torrent::util::from_hex;
 
 fn make_client_hello() -> ClientHello {
+    let random = from_hex("1a87a2e2f77536fcfa071500af3c7dffa5830e6c61214e2dee7623c2b925aed8").unwrap();
+    let session_id = from_hex("7d954b019486e0dffaa7769a4b9d27d796eaee44b710f18d630f3292b6dc7560").unwrap();
+    println!("random.len() = {}", random.len());
+    println!("session_id.len() = {}", session_id.len());
+    assert!(random.len() == 32);
+    assert!(session_id.len() == 32);
+
+    let mut random_fixed: [u8; 32] = Default::default();
+    random_fixed.copy_from_slice(&random);
+
+    let mut cipher_suites = Vec::<CipherSuite>::new();
+    cipher_suites.push(CipherSuite::TLS_AES_256_GCM_SHA384);
+    cipher_suites.push(CipherSuite::TLS_CHACHA20_POLY1305_SHA256);
+    cipher_suites.push(CipherSuite::TLS_AES_128_GCM_SHA256);
+    cipher_suites.push(CipherSuite::Unknown(0x00ff));
+
+    let mut extensions = vec![
+        Extension::ServerName(vec![ServerName::HostName(String::from("localhost"))]),
+        Extension::ECPointFormats(vec![
+            ECPointFormat::Uncompressed,
+            ECPointFormat::ANSIX962CompressedPrime,
+            ECPointFormat::ANSIX962CompressedChar2]),
+        Extension::EllipticCurves(vec![
+            NamedCurve::X25519,
+            NamedCurve::Secp256r1,
+            NamedCurve::X448,
+            NamedCurve::Secp521r1,
+            NamedCurve::Secp384r1]),
+        Extension::NextProtocolNegotiation(vec![]),
+        Extension::ApplicationLayerProtocolNegotiation(vec![
+            ProtocolName { data: Vec::from("h2".as_bytes()) },
+            ProtocolName { data: Vec::from("http/1.1".as_bytes()) },
+            ]),
+        Extension::EncryptThenMac,
+        Extension::ExtendedMasterSecret,
+        Extension::PostHandshakeAuth,
+        Extension::SignatureAlgorithms(vec![
+            SignatureScheme::EcdsaSecp256r1Sha256,
+            SignatureScheme::EcdsaSecp384r1Sha384,
+            SignatureScheme::EcdsaSecp521r1Sha512,
+            SignatureScheme::Ed25519,
+            SignatureScheme::Ed448,
+            SignatureScheme::RsaPssPssSha256,
+            SignatureScheme::RsaPssPssSha384,
+            SignatureScheme::RsaPssPssSha512,
+            SignatureScheme::RsaPssRsaeSha256,
+            SignatureScheme::RsaPssRsaeSha384,
+            SignatureScheme::RsaPssRsaeSha512,
+            SignatureScheme::RsaPkcs1Sha256,
+            SignatureScheme::RsaPkcs1Sha384,
+            SignatureScheme::RsaPkcs1Sha512]),
+        Extension::SupportedVersions(vec![2, 3, 4]),
+        Extension::PskKeyExchangeModes(vec![PskKeyExchangeMode::PskDheKe]),
+        Extension::KeyShareClientHello(vec![
+            KeyShareEntry {
+                group: NamedGroup::X25519,
+                key_exchange: from_hex("13676e955e3f1e389274ffb25c6adb258a549c56779fd593613a73ea85acc669").unwrap().to_vec()
+            }])
+    ];
+
+
     ClientHello {
         legacy_version: 0x0303,
-        random: Default::default(), // TODO
-        legacy_session_id: Vec::new(), // TODO
-        cipher_suites: Vec::new(), // TODO
-        legacy_compression_methods: Vec::new(), // TODO
-        extensions: Vec::new(), // TODO
+        random: random_fixed,
+        legacy_session_id: session_id,
+        cipher_suites: cipher_suites,
+        legacy_compression_methods: vec![0],
+        extensions: extensions,
     }
 }
 
 async fn test_client() -> Result<(), Box<dyn Error>> {
+    let client_hello = make_client_hello();
+    let handshake = Handshake::ClientHello(client_hello);
+
+    let mut writer = BinaryWriter::new();
+    writer.write_item(&handshake)?;
+
+    let output_record = TLSPlaintext {
+        content_type: ContentType::Handshake,
+        legacy_record_version: 0x0301,
+        fragment: &Vec::<u8>::from(writer),
+    };
+
+    let serialized_filename = "record-constructed.bin";
+    std::fs::write(serialized_filename, &output_record.to_vec())?;
+    println!("Wrote {}", serialized_filename);
+
     Ok(())
 }
 
