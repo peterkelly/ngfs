4ae20f45c97acf027cea66c092a519065355e092
Peter Kelly <peter@pmkelly.net>
2021-04-28 20:29:18 +0700

    Work

--- src/bin/ipfs.rs
+++ src/bin/ipfs.rs
@@ -7,6 +7,8 @@
 
 // https://github.com/libp2p/specs/blob/master/connections/README.md#connection-upgrade
 
+use core::pin::Pin;
+use core::task::{Context, Poll};
 use std::error::Error;
 use std::net::SocketAddr;
 use std::fmt;
@@ -14,7 +16,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio::time::sleep;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt, AsyncRead, AsyncWrite, BufReader, BufWriter};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, AsyncRead, AsyncWrite, BufReader, BufWriter, ReadBuf};
 use tokio::sync::{Notify};
 use futures::future::join;
 use torrent::util::{from_hex, escape_string, vec_with_len, BinaryData, DebugHexDump, Indent};
@@ -134,77 +136,148 @@ enum ConnectionError {
     Other(String),
 }
 
+enum ReaderAction {
+    Read,
+    Exit,
+    Wait,
+}
+
+enum WriterAction {
+    Read,
+    Exit,
+    Wait,
+}
+
 struct ConnectionInner {
+    session: rustls::ClientSession,
+    incoming: IncomingData,
+    outgoing: OutgoingData,
+    error: Option<ConnectionError>,
+    done: bool,
+}
+
+impl ConnectionInner {
+    fn read_tls(&mut self) -> Result<usize, std::io::Error> {
+        self.session.read_tls(&mut self.incoming)
+        // unimplemented!()
+    }
 }
 
 struct Connection {
-    session: Mutex<rustls::ClientSession>,
-    incoming: Mutex<IncomingData>,
-    outgoing: Mutex<OutgoingData>,
+    inner: Mutex<ConnectionInner>,
     read_not: Notify,
     write_not: Notify,
-    error: Mutex<Option<ConnectionError>>,
-    done: Mutex<bool>,
 }
 
 impl Connection {
     fn new(session: rustls::ClientSession) -> Self {
         Connection {
-            session: Mutex::new(session),
-            incoming: Mutex::new(IncomingData::new()),
-            outgoing: Mutex::new(OutgoingData::new()),
+            inner: Mutex::new(ConnectionInner {
+                session: session,
+                incoming: IncomingData::new(),
+                outgoing: OutgoingData::new(),
+                error: None,
+                done: false,
+            }),
             read_not: Notify::new(),
             write_not: Notify::new(),
-            error: Mutex::new(None),
-            done: Mutex::new(false),
+        }
+    }
+
+    fn reader_action(&self) -> ReaderAction {
+        let inner = self.inner.lock().unwrap();
+        if inner.done {
+            ReaderAction::Exit
+        }
+        else if inner.session.wants_read() {
+            ReaderAction::Read
+        }
+        else {
+            ReaderAction::Wait
+        }
+    }
+
+    fn writer_action(&self) -> WriterAction {
+        let inner = self.inner.lock().unwrap();
+        if inner.done {
+            WriterAction::Exit
+        }
+        else if inner.session.wants_read() {
+            WriterAction::Read
+        }
+        else {
+            WriterAction::Wait
         }
     }
 
     fn get_error(&self) -> Option<ConnectionError> {
-        self.error.lock().unwrap().clone()
-    }
-
-    fn set_error(&self, error: Option<ConnectionError>) {
-        *self.error.lock().unwrap() = error;
+        self.inner.lock().unwrap().error.clone()
     }
 
     fn wants_read(&self) -> bool {
-        self.session.lock().unwrap().wants_read()
+        self.inner.lock().unwrap().session.wants_read()
     }
 
     fn on_receive_close(&self) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.done = true;
+        self.read_not.notify_one();
+        self.write_not.notify_one();
     }
 
-    fn on_receive_error(&self) {
+    fn on_receive_error(&self, e: std::io::Error) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.error = Some(ConnectionError::Other(format!("{}", e)));
+        inner.done = true;
+        self.read_not.notify_one();
+        self.write_not.notify_one();
     }
 
     fn on_receive_data(&self, data: &[u8]) {
-        let mut incoming = self.incoming.lock().unwrap();
-        let mut session = self.session.lock().unwrap();
-        incoming.data.extend_from_slice(data);
-        match session.read_tls(&mut *incoming) {
-            Ok(res) => {
-                // unimplemented!()
-            }
-            Err(e) => {
-                self.set_error(Some(ConnectionError::Other(format!("{}", e))));
-                *self.done.lock().unwrap() = true;
-                return;
-            }
+        let mut inner = self.inner.lock().unwrap();
+        inner.incoming.data.extend_from_slice(data);
+        if let Err(e) = inner.read_tls() {
+            inner.error = Some(ConnectionError::Other(format!("{}", e)));
+            inner.done = true;
+            return;
         };
-        match session.process_new_packets() {
-            Ok(res) => {
-                // unimplemented!()
-            }
-            Err(e) => {
-                self.set_error(Some(ConnectionError::TLSError(e)));
-                *self.done.lock().unwrap() = true;
-                return;
-            }
+        if let Err(e) = inner.session.process_new_packets() {
+            inner.error = Some(ConnectionError::TLSError(e));
+            inner.done = true;
+            return;
         };
-        if session.wants_write() {
+        if inner.session.wants_write() {
             self.write_not.notify_one();
         }
+    }
+
+    // pub async fn write(&mut self, buf: &[u8]) {
+    //     let mut inner = self.inner.lock().unwrap();
+    //     match inner.session.write_all(buf) {
+    //         Ok(res) => {
+    //         }
+    //         Err(e) => {
+    //         }
+    //     }
+    // }
+}
+
+impl AsyncRead for Connection {
+    fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut ReadBuf<'_>) -> Poll<std::io::Result<()>> {
+        unimplemented!()
+    }
+}
+
+impl AsyncWrite for Connection {
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize, std::io::Error>> {
+        unimplemented!()
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        unimplemented!()
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
         unimplemented!()
     }
 }
@@ -214,46 +287,41 @@ async fn connection_reader<'a>(conn: Arc<Connection>, mut reader: impl AsyncRead
     let mut count = 0;
     // println!("connection_reader: start");
     loop {
-        // let wants_read = conn.session.lock().unwrap().wants_read();
-        if conn.wants_read() {
-
-            let mut buf: [u8; READ_SIZE] = [0; READ_SIZE];
-            loop {
-                match reader.read(&mut buf).await {
-                    Ok(0) => {
-                        // println!("{}: Connection closed", conn.connection_id);
-                        conn.on_receive_close();
-                        break;
-                    }
-                    Ok(r) => {
-                        // let s = String::from_utf8_lossy(&buf[..r]);
-                        // println!("{}: Received {} bytes: {}", conn.connection_id, r, escape_string(&s));
-                        // state.on_receive(&conn, &buf[..r]).await;
-                        conn.on_receive_data(&buf[..r]);
-                    }
-                    Err(e) => {
-                        // println!("{}: Connection error: {}", conn.connection_id, e);
-                        conn.on_receive_error();
-                        break;
-                    }
-                };
-            }
-
-        }
         println!("connection_reader: count = {}", count);
-
-        conn.read_not.notified().await;
         count += 1;
 
-    //     match conn.read_sem.acquire().await {
-    //         Ok(x) => {
-    //             println!("connection_reader: Acquired permit");
-    //         }
-    //         Err(e) => {
-    //             println!("connection_reader: Failed to acquire permit");
-    //             return;
-    //         }
-    //     }
+        match conn.reader_action() {
+            ReaderAction::Read => {
+                let mut buf: [u8; READ_SIZE] = [0; READ_SIZE];
+                loop {
+                    match reader.read(&mut buf).await {
+                        Ok(0) => {
+                            // println!("{}: Connection closed", conn.connection_id);
+                            conn.on_receive_close();
+                            break;
+                        }
+                        Ok(r) => {
+                            // let s = String::from_utf8_lossy(&buf[..r]);
+                            // println!("{}: Received {} bytes: {}", conn.connection_id, r, escape_string(&s));
+                            // state.on_receive(&conn, &buf[..r]).await;
+                            conn.on_receive_data(&buf[..r]);
+                        }
+                        Err(e) => {
+                            // println!("{}: Connection error: {}", conn.connection_id, e);
+                            conn.on_receive_error(e);
+                            break;
+                        }
+                    };
+                }
+
+            }
+            ReaderAction::Exit => {
+                return;
+            }
+            ReaderAction::Wait => {
+                conn.read_not.notified().await;
+            }
+        }
     }
 }
 
