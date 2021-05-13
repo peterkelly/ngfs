5a29bd158545e725e65b1d12014eedf1cf488663
Peter Kelly <peter@pmkelly.net>
2021-05-13 22:29:38 +0700

    TLS: Add protocol directory

--- src/tls/mod.rs
+++ src/tls/mod.rs
@@ -1,3 +1,4 @@
 pub mod types;
 pub mod helpers;
 pub mod error;
+pub mod protocol;
---
+++ src/tls/protocol/server.rs
@@ -0,0 +1,6 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_mut)]
+#![allow(unused_assignments)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
